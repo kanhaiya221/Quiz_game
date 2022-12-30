ce8179eb91da40c248b5cedf43326315fa199125
kanhaiya221 <42540214+kanhaiya221@users.noreply.github.com>
2022-12-30 13:21:07 +0530

    Add files via upload

---
+++ quiz_brain.py
@@ -0,0 +1,30 @@
+class QuizBrain:
+
+    def __init__(self,question_list):
+        self.question_number = 0
+        self.current_score = 0
+        self.question_list = question_list
+
+
+    def still_has_question(self):
+        while self.question_number != len(self.question_list):
+            return(self.question_number <len(self.question_list))
+
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right")
+            self.current_score +=1
+
+        else:
+            print("Thats wrogn answer")
+
+        print(f"correct answer is {correct_answer} ")
+        print(f"Your current score is:{self.current_score}/{self.question_number} ")
